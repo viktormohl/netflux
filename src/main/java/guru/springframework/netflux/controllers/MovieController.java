c8b44f0c718e530599288dbc0c7b4cab52baa633
v.mohl <viktor.mohl@gmail.com>
2021-05-29 01:35:53 +0200

    add MovieEvent

--- src/main/java/guru/springframework/netflux/controllers/MovieController.java
+++ src/main/java/guru/springframework/netflux/controllers/MovieController.java
@@ -1,8 +1,10 @@
 package guru.springframework.netflux.controllers;
 
 import guru.springframework.netflux.domain.Movie;
+import guru.springframework.netflux.domain.MovieEvent;
 import guru.springframework.netflux.services.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +20,17 @@ public class MovieController {
     private final MovieService movieService;
 
     @GetMapping("/{id}")
-    Mono<Movie> getMovieById(@PathVariable String id){
+    Mono<Movie> getMovieById(@PathVariable String id) {
         return movieService.getMovieById(id);
     }
 
     @GetMapping
-    Flux<Movie> getAllMovies(){
+    Flux<Movie> getAllMovies() {
         return movieService.getAllMovies();
+    }
+
+    @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<MovieEvent> streamMovieEvents(@PathVariable String id) {
+        return movieService.streamMovieEvents(id);
     }
 }
